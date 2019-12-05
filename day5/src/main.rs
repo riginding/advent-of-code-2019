a8e37b973ce4759b989d3eeae302f1565ede4ead
Ryan Riginding <ryan@riginding.com>
2019-12-05 15:37:18 +0100

    day5 - part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,12 @@
 use ::std::convert::TryFrom;
 
 fn main() -> std::io::Result<()> {
-    let mut input = get_input()?;
+    let input = get_input()?;
+
+    let mut program = IntCode::new(input.into());
+    program.execute();
+
+    println!("1: {}", program);
 
     Ok(())
 }
@@ -10,16 +15,24 @@ fn main() -> std::io::Result<()> {
 enum OpCode {
     Add {
         input_a: usize,
+        immediate_a: bool,
+
         input_b: usize,
+        immediate_b: bool,
+
         output: usize,
     },
     Multiply {
         input_a: usize,
+        immediate_a: bool,
+
         input_b: usize,
+        immediate_b: bool,
+
         output: usize,
     },
     Save {
-      address: usize,
+        address: usize,
     },
     Load {
         address: usize,
@@ -31,21 +44,36 @@ impl TryFrom<Vec<i32>> for OpCode {
     type Error = &'static str;
 
     fn try_from(value: Vec<i32>) -> Result<Self, Self::Error> {
-        match value[0] {
-            1 => Ok(OpCode::Add {
+        match parse_instruction(value[0]) {
+            (1, a, b) => Ok(OpCode::Add {
                 input_a: value[1] as usize,
+                immediate_a: a,
                 input_b: value[2] as usize,
+                immediate_b: b,
                 output: value[3] as usize,
             }),
-            2 => Ok(OpCode::Multiply {
+            (2, a, b) => Ok(OpCode::Multiply {
                 input_a: value[1] as usize,
+                immediate_a: a,
                 input_b: value[2] as usize,
+                immediate_b: b,
                 output: value[3] as usize,
             }),
-            99 => Ok(OpCode::Done),
+            (3, _, _) => Ok(OpCode::Save {
+                address: value[1] as usize,
+            }),
+            (4, _, _) => Ok(OpCode::Load {
+                address: value[1] as usize,
+            }),
+            (99, _, _) => Ok(OpCode::Done),
             _ => Err("no such code"),
         }
     }
+
+}
+
+fn parse_instruction(instruction: i32) -> (i32, bool, bool) {
+    (instruction%100, instruction/100%10 == 1, instruction/1000%10 == 1)
 }
 
 #[derive(Debug)]
@@ -81,8 +109,23 @@ impl IntCode {
     }
 
     fn get_instructions(&mut self) -> Vec<i32> {
-        let result = &self.data[self.cursor_position..=self.cursor_position + 3];
-        self.cursor_position = self.cursor_position + 4;
+        let instruction: i32 = self.data[self.cursor_position] % 10;
+        let mut result = self.data.as_slice();
+        match instruction {
+            1 | 2 => {
+                result = &self.data[self.cursor_position..=self.cursor_position + 3];
+                self.cursor_position = self.cursor_position + 4;
+            }
+            3 | 4 => {
+                result = &self.data[self.cursor_position..=self.cursor_position + 1];
+                self.cursor_position = self.cursor_position + 2;
+            }
+            9 => {
+                result = &self.data[self.cursor_position..=self.cursor_position];
+                self.cursor_position = self.cursor_position + 1;
+            }
+            _ => panic!("danger"),
+        }
 
         result.into()
     }
@@ -94,14 +137,40 @@ impl IntCode {
             OpCode::Done => self.finished = true,
             OpCode::Add {
                 input_a,
+                immediate_a,
                 input_b,
+                immediate_b,
                 output,
-            } => self.data[output] = self.data[input_a] + self.data[input_b],
+            } => {
+                self.data[output] = if immediate_a {
+                    input_a as i32
+                } else {
+                    self.data[input_a]
+                } + if immediate_b {
+                    input_b as i32
+                } else {
+                    self.data[input_b]
+                }
+            }
             OpCode::Multiply {
                 input_a,
+                immediate_a,
                 input_b,
+                immediate_b,
                 output,
-            } => self.data[output] = self.data[input_a] * self.data[input_b],
+            } => {
+                self.data[output] = if immediate_a {
+                    input_a as i32
+                } else {
+                    self.data[input_a]
+                } * if immediate_b {
+                    input_b as i32
+                } else {
+                    self.data[input_b]
+                }
+            }
+            OpCode::Save { address } => self.data[address] = 1,
+            OpCode::Load { address } => println!("{}", self.data[address]),
         };
     }
 }
@@ -116,7 +185,6 @@ fn get_input() -> std::io::Result<Vec<i32>> {
 
     Ok(data)
 }
-
 
 #[cfg(test)]
 mod tests {
