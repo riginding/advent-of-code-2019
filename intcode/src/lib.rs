98afd01a10e130c5aa2df4e12e4c8e1498cc4319
Ryan Riginding <ryan@riginding.com>
2019-12-09 23:10:46 +0100

    intcode refactor ideas

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,3 +1,13 @@
+/// IntCode
+/// 
+/// TODO:
+/// - [ ] multiple Instances of IntCode can be run on different threads
+/// - [ ] communication between IntCodes (input queues) should happen over Channels.
+/// - [ ] Should be prgrammed in a way that it works for all days
+/// - [ ] Try to write in idiomatic rust
+/// - [ ] Handle all Errors properly no unwrapping
+/// - [ ] Provide Test Suite
+/// - [ ] use better variable names
 #[cfg(test)]
 mod tests {
     #[test]
